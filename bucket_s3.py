21c95bdf48e0bd2c28870d90306f1edd922327b8
Kwanhathai KSKSIN <kwanhatai.sin@gmail.com>
2025-01-21 15:46:32 +0700

    sample_secrets-gg-internal_apis

--- bucket_s3.py
+++ bucket_s3.py
@@ -10,6 +10,7 @@ def aws_upload(data: Dict):
     database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
     database.push(data)
 
+
 def transform_data(es_data: Dict) -> Dict:
     es_data = {**data, "origin": "ES"}
 
@@ -17,6 +18,7 @@ MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/te
 
 def pull_data_from_mongo(query: Dict):
     return pymongo.connect(MONGO_URI).fetch(query)
+
 
 def push_mongo_to_s3(query):
     for element in pull_data_from_mongo(query):
